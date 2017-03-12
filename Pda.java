00d43d1714499a1421e20ddec292450661f6102b
amd61 <arlenem.mcdaid@googlemail.com>
2017-03-12 18:22:38 +0000

    Fix static testing issues

--- Pda.java
+++ Pda.java
@@ -1,13 +1,13 @@
 class Pda {
   private boolean func1(int val){
-    if(val = 1){
+    if(val == 1){ // was assignment operator, should be == operator
       return true;
     }else{
       return false;
     }
   }
 
-  privat int max(int a int b){
+  private int max(int a, int b){ // typo in private (missing e), comma needed between a and b
     if(a > b){
       return a;
     }else{
@@ -17,18 +17,19 @@ class Pda {
 
   private int looper(){
     int i = 0;
-    while(i <= 10){
+    while(i < 10){ // should be <10, or it will count to 11 instead
       i++;
-      return i;
     }
+      return i; // return i outside the loop or the loop only runs once
   } 
 
-   public void checkLoop(){
-  if (looper == 10){
-    return "looper passed"
+   public String checkLoop(){ // return a string, not void
+  if (looper() == 10){ // looper is a function, not a variable, so call it
+    return "looper passed"; // missing semicolon at end of line
   }
   else{
     return "looper failed";
   }
   
 }
+} // missing close bracket
