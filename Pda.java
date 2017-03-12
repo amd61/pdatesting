2995dcb94fb138feb8054652d4af8a68b36370ab
amd61 <arlenem.mcdaid@googlemail.com>
2017-03-12 18:15:38 +0000

    Original

---
+++ Pda.java
@@ -0,0 +1,34 @@
+class Pda {
+  private boolean func1(int val){
+    if(val = 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  privat int max(int a int b){
+    if(a > b){
+      return a;
+    }else{
+      return b;
+    }
+  }
+
+  private int looper(){
+    int i = 0;
+    while(i <= 10){
+      i++;
+      return i;
+    }
+  } 
+
+   public void checkLoop(){
+  if (looper == 10){
+    return "looper passed"
+  }
+  else{
+    return "looper failed";
+  }
+  
+}
